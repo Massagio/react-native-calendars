deb5a5d41907a52015be487bcd8f470b0ae90f16
Daniel Fiebig <daniel@massagio.de>
2017-08-30 23:13:26 +0200

    feat(language): add language support de

--- src/dateutils.js
+++ src/dateutils.js
@@ -1,5 +1,14 @@
 const XDate = require('xdate');
 
+XDate.locales.de = {
+      monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+        monthNamesShort: ['Jan.', 'Feb.', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul.', 'Aug', 'Sep.', 'Okt.', 'Nov.', 'Dez.'],
+          dayNames: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
+            dayNamesShort: ['Mo.', 'Di.', 'Mi.', 'Do.', 'Fr.', 'Sa.', 'So.']
+};
+
+XDate.defaultLocale = 'de';
+
 function sameMonth(a, b) {
   return a instanceof XDate && b instanceof XDate &&
     a.getFullYear() === b.getFullYear() &&
